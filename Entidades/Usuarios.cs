3292732ad5f6597981e1dabc5f5f5166fe21dced
vismarlora <vismar_lora@ucne.edu.do>
2021-09-19 16:58:25 -0400

    Tarea3RegistroUsuarios

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroUsuariosWPF.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+        public string Alias { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public bool Estado { get; set; }
+        public int RolId { get; set; }
+
+        [ForeignKey("RolId")]
+        public virtual Roles Rol { get; set; }
+    }
+}
